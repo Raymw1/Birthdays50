bd069bda7064e8588327a9f04f763e7bf5cbc37d
Rayan Wilbert <rayan.rmw@gmail.com>
2021-03-22 16:36:49 -0300

    Login finished(I think)

--- application.py
+++ application.py
@@ -119,25 +119,24 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         username = request.form.get("username")
-        password = request.form.get("password")
+        password = request.form.get("pwd")
         # Ensure username was submitted
         if not username:
-            return apology("must provide username", 403)
+            return apology("Provide an username", 403)
 
         # Ensure password was submitted
         elif not password:
-            return apology("must provide password", 403)
+            return apology("Provide a password", 403)
 
         rows = db.execute("SELECT * FROM users WHERE username = ?", username)
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-            return apology("invalid username and/or password", 403)
+            return apology("Provide a valid username and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
         # Redirect user to home page
         return redirect("/")
-
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
